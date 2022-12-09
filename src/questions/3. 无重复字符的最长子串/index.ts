b48720642c99147f9b8658b1fead558852b47be1
Eve-Sama <948832626@qq.com>
2022-12-09 18:14:10 +0800

    refactor: 优化第三题

--- src/questions/3. 无重复字符的最长子串/index.ts
+++ src/questions/3. 无重复字符的最长子串/index.ts
@@ -1,55 +1,30 @@
-// 使用Set
 function lengthOfLongestSubstring(s: string): number {
+  if (s.length === 0) {
+    return 0;
+  }
+  if (s.length === 1) {
+    return 1;
+  }
   let max: number = 0;
-  const set = new Set<string>();
-  for (const str of s) {
-    const exist = set.has(str);
-    // 当set中存在重复值时, 需要重新设置set为重复值之后一个元素开始的字符串
-    if (exist) {
-      const list = Array.from(set);
-      set.clear();
-      let startAdd = false;
-      list.forEach(existStr => {
-        if (startAdd) {
-          set.add(existStr);
-        }
-        if (existStr === str) {
-          startAdd = true;
-        }
-      });
+  let i = 0;
+  let j = 0;
+  while (j < s.length) {
+    const rightCurrentStr = s.charAt(j);
+    const existIndex = s.indexOf(rightCurrentStr, i);
+    // 存在重复的变量且在ij之间
+    if (j !== 0 && existIndex >= i && existIndex < j) {
+      i = existIndex + 1;
+    } else {
+      j++;
     }
-    set.add(str);
-    max = max > set.size ? max : set.size;
+    const validStr = s.slice(i, j);
+    max = Math.max(max, validStr.length);
   }
   return max;
 }
-// 使用数组循环字符串
-// function lengthOfLongestSubstring(s: string): number {
-//   const list = s.split('');
-//   let maxLengthList: string[] = [];
-//   let maxLengthValue = 0;
-//   list.forEach(str => {
-//     const existIndex = maxLengthList.findIndex(v => v === str);
-//     // 存在元素
-//     if (existIndex !== -1) {
-//       const tempStrList: string[] = [];
-//       for (let index = existIndex + 1; index < maxLengthList.length; index++) {
-//         const element = maxLengthList[index];
-//         tempStrList.push(element);
-//       }
-//       maxLengthList = [...tempStrList, str];
-//       maxLengthValue = maxLengthValue > tempStrList.length ? maxLengthValue : tempStrList.length;
-//     } else {
-//       // 不存在元素
-//       maxLengthList.push(str);
-//       maxLengthValue = maxLengthValue > maxLengthList.length ? maxLengthValue : maxLengthList.length;
-//     }
-//   });
-//   return maxLengthValue;
-// }
 
 function test() {
-  const res = lengthOfLongestSubstring('dvdf');
+  const res = lengthOfLongestSubstring('abcabcbb');
   console.log(res);
 }
 
